a94e1f7a204ce5dc02f713fb1cb8f33a63f86681
DrakeBackend <drake@fusionsynclabs.com>
2025-01-03 15:28:57 +0500

    chore: create react app && setup

---
+++ Whatsapp_FrontEnd/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ChatIcon } from './svg'
+
+const App = () => {
+  return (
+    <div>
+      Here is a frontend app
+      <ChatIcon className="red" />
+    </div>
+  )
+}
+
+export default App

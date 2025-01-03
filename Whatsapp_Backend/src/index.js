80c5f73700ffc3a7c3c8ad2436066f8e85f94d08
DrakeBackend <drake@fusionsynclabs.com>
2025-01-03 15:08:35 +0500

    mongooDb functionality and debugs connections

--- Whatsapp_Backend/src/index.js
+++ Whatsapp_Backend/src/index.js
@@ -1,8 +1,37 @@
+import mongoose from 'mongoose';
 import app from './app.js';
 import logger from "./configs/logger.config.js"
+import 'colors';
 
-// Envs
-const port = process.env.PORT || 5000
+// Envs Variables
+const { DATABASE_URL, PORT, NODE_ENV } = process.env
+const port = PORT || 5000
+
+// exit on mongodb error 
+mongoose.connection.on("error", (err) => {
+    logger.error(`Mongodb connection error : ${err}`);
+    process.exit(1);
+})
+
+// mongodb debug mode
+if (NODE_ENV !== "production") {
+    mongoose.set('debug', true)
+}
+
+
+// mongoDb Connection
+// required libs : mongoose | colors
+// run the following command
+// npm i mongoose colors
+
+if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in the environment variables.");
+}
+
+// MongoDB Connection
+mongoose.connect(DATABASE_URL)
+    .then(() => logger.info(`> Connected... ${NODE_ENV !== 'production' ? DATABASE_URL : ""}`.bgCyan))
+    .catch(err => logger.error(`> Error while connecting to MongoDB: ${err.message}`.underline.red));
 
 // !important! 
 // you need to install the following libraries |express|[dotenv > if required]
